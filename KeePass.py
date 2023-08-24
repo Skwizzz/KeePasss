6c079cdc35051c71030a0f3e637a307fc9cd6794
Skwizz <72879590+Skwizzz@users.noreply.github.com>
2023-08-25 00:16:26 +0200

    Update KeePass.py
    Added the UI for the encoder

--- KeePass.py
+++ KeePass.py
@@ -1,36 +1,68 @@
+from tkinter import *
 import os
 import cryptocode
 import pyperclip
-verif = "no"
-mode = input(" Would you like to read or write a file ? (r/w)" )
-if mode == "w":
-    name = input("Name of the file ? (Usually the name of the app)" ) 
-    while verif=="no":
-        passw = input("What is your password ? : ")
-        print(" Please verify that your password is",passw,"(yes/no)" )
-        verif = input()
-    passwencoded = cryptocode.encrypt(passw,"Key") # This is the Key used to encrypt and decrypt, if you are using this application for a personal use you should change this. This is your "main password".
-    file = open(name,"w")
-    file.write(passwencoded)
-    file.close()
-    print("Done, press ENTER to exit")
-    input()
-if mode == "r":
-    name = input("Name of the file ? (Usually the name the app)")
-    with open(name,"r+")as f:
-        print("Your password is")
-        passw = f.read()
-        passwdecoded = cryptocode.decrypt(passw,"Key")
-        
-        print(passwdecoded)
+from tkinter import filedialog
+def save_data():
+    passw_info = passw.get()
+    file_info = fileinfo.get()
+    passwcoded = cryptocode.encrypt(passw_info,"KeePassKey25152021")
+    f = open(file_info,"w")
+    f.write(passwcoded)
+    f.close()
 
 
-        copy = input(" Would you like to copy the password to clipboard ? (y/n) ")
-        if copy =="y":
-            pyperclip.copy(passwdecoded)
-            print("Copied to clipboard")
-        else:
-            input("Done.")
-        f.close()
-    print("Please press ENTER to exit")    
-    input()
+
+
+root = Tk()
+root.title('KeePass')
+root.geometry('600x400')
+root.configure(bg="black")
+root.resizable(0,0)
+
+
+
+
+
+fileinfo = StringVar()
+passw = StringVar()
+
+   
+    
+    
+Label(root,text="Application ? ",bg="black",fg="white",font=29).pack()
+Entry(root,relief=FLAT,width=50,textvariable=fileinfo).pack()
+    
+  
+Label(root,text="Password ? ",bg="black",fg="white",font=29).pack()
+Entry(root,relief=FLAT,width=50,show="*",textvariable=passw).pack()
+    
+   
+save = Button(root,text="SAVE",bg="white",bd=0,width=15,command=lambda:save_data()).place(y=100,x=248)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
