9dcfd8f3c606f7b1b6acc4844a5f63ed1c725cee
Skwizz <72879590+Skwizzz@users.noreply.github.com>
2023-08-24 12:02:10 +0200

    Add files via upload

---
+++ KeePass.py
@@ -0,0 +1,36 @@
+import os
+import cryptocode
+import pyperclip
+verif = "no"
+mode = input(" Would you like to read or write a file ? (r/w)" )
+if mode == "w":
+    name = input("Name of the file ? (Usually the name of the app)" ) 
+    while verif=="no":
+        passw = input("What is your password ? : ")
+        print(" Please verify that your password is",passw,"(yes/no)" )
+        verif = input()
+    passwencoded = cryptocode.encrypt(passw,"KeePassKey25152021")
+    file = open(name,"w")
+    file.write(passwencoded)
+    file.close()
+    print("Done, press ENTER to exit")
+    input()
+if mode == "r":
+    name = input("Name of the file ? (Usually the name the app)")
+    with open(name,"r+")as f:
+        print("Your password is")
+        passw = f.read()
+        passwdecoded = cryptocode.decrypt(passw,"KeePassKey25152021")
+        
+        print(passwdecoded)
+
+
+        copy = input(" Would you like to copy the password to clipboard ? (y/n) ")
+        if copy =="y":
+            pyperclip.copy(passwdecoded)
+            print("Copied to clipboard")
+        else:
+            input("Done.")
+        f.close()
+    print("Please press ENTER to exit")    
+    input()
